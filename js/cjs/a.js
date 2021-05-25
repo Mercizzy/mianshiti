c82a5901b2d7857c9e73804e5f27f50f91e66b56
merci <1978427972@qq.com>
2021-05-26 00:02:30 +0800

    init3

--- js/cjs/a.js
+++ js/cjs/a.js
@@ -1,2 +1,8 @@
-let b = require('./b')
-console.log(b);
+let {a} = require('./c')
+let obj = require('./b')
+console.log(obj);
+console.log(a);
+obj.b = 2
+console.log(obj);
+a = 11
+console.log(a);
---
+++ js/cjs/c.js
@@ -0,0 +1,8 @@
+
+let a = 10
+
+exports.a = a;
+
+setTimeout(() => {
+  console.log(a);
+}, 2000)
